efc829a5fe61d613bbae174c6264eb28d6fffbda
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-10 15:38:14 -0700

    type hints & uper for Tuesday assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,46 @@
+from typing import List, Tuple
+Lineage = Tuple[int, int]
+Ancestor_Graph = List[Lineage]
+""" UPER
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+Understand: The input is a list of tuples that are one way edges between parent
+child relationships. Each node in the graph is identified with their unique int
+
+We have to write a function that takes a graph, a node, and returns the earliest
+known ancestor which is the furthest node away. In case of ties, return the
+one with the smallest number and return -1 if nothing is found.
+
+Plan: The graph is a directed acyclic graph. The most straight forward thing to
+do seems to be to do a DFS and for debugging purposes probably keep a list of
+list of integers which will represent a lineage path. Then filter by longest
+length of a lineage and then by whichever ancestor is the earliest. 
+
+Negative one is returned when either a person with no children is the input or
+a person who does not exist in the graph is input. 
+
+    '''
+       10
+     /
+    1   2   4  11
+     \ /   / \ /
+      3   5   8
+       \ / \   \
+        6   7   9
+    '''
+
+In our example graph which is illustrated above and in dict form below:
+
+{1: 3, 2: 3, 3: 6, 5: 7, 4: 8, 8: 9, 11: 8, 10: 1}
+
+10, 2, 4, 11 would return -1. They do not exist among the values of the dictionary
+
+Actually, since this is deterministic, it might be better to 
+"""
+def earliest_ancestor(ancestors: Ancestor_Graph, starting_node: int) -> int:
+    family_tree = dict(ancestors)
+
+
+
+test_ancestors = dict([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)])
+print(test_ancestors)
+
